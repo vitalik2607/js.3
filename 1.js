8ecbc8a2650718944fa728f2a678978c9a99e475
Vitalik2607 <vitalbmx2607@gmail.com>
2020-04-07 15:19:05 +0300

    add

---
+++ 1.js
@@ -0,0 +1,9 @@
+//1) Подсчитать сумму всех чисел в заданном пользователем диапазоне. 
+let numberOne = +prompt("Введите начальное число");
+let numberTwo = +prompt("Введите конечное число");
+let sum = 0;
+while (numberOne <= numberTwo) {
+    sum += numberOne;
+    numberOne++;
+}
+alert(`сумма всех чисел в диапазоне от ${numberOne} до ${numberTwo} равна ${sum}`);
